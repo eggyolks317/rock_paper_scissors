60f47cafc50340549e86d0f760b048f24974f12b
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-23 21:15:04 -0700

    finished basic js and styling functioning properly

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let humanChoice;
         let scissor = document.querySelector('#scissor');
         let humanScore = document.querySelector('#human');
         let computerScore = document.querySelector('#computer');
+        let result = document.querySelector('#result');
         function getComputerChoice() {
             let choice = randomNum(3);
             if (choice == 0) {
@@ -22,16 +23,19 @@ let humanChoice;
         function loseMessage(computerChoice = getComputerChoice(), humanChoice) {
             //returns a lose message and plus 1 to computer score
             computerScore.textContent++;
-            return 'You lose, ' + computerChoice + ' beats ' +humanChoice;
+            result.textContent = 'You lose, ' + computerChoice + ' beats ' +humanChoice;
+            return result.textContent;
         }
         function winMessage(computerChoice = getComputerChoice(), humanChoice) {
             //returns a win message and plus 1 to human score
             humanScore.textContent++;
-            return 'You win, ' + humanChoice + ' beats ' +computerChoice;
+            result.textContent =  'You win, ' + humanChoice + ' beats ' +computerChoice;
+            return result.textContent;
         }
         function drawMessage(computerChoice = getComputerChoice(), humanChoice) {
             //returns a draw message
-            return 'Draw!';
+            result.textContent = 'Draw!';
+            return result.textContent;
         }
 
         //game functions
@@ -42,23 +46,39 @@ let humanChoice;
             //game logic
             if (computerChoice == 'rock' && humanChoice == 'scissor') {
                 console.log(loseMessage(computerChoice, humanChoice));
-            } else if (computerChoice == 'scissor' && humanChoice == 'paper') {
+            } 
+            else if (computerChoice == 'scissor' && humanChoice == 'paper') {
                 console.log(loseMessage(computerChoice, humanChoice));
-            } else if (computerChoice == 'paper' && humanChoice == 'rock') {
+            } 
+            else if (computerChoice == 'paper' && humanChoice == 'rock') {
                 console.log(loseMessage(computerChoice, humanChoice));
-            } else if (computerChoice == 'rock' && humanChoice == 'rock') {
+            } 
+            else if (computerChoice == 'rock' && humanChoice == 'rock') {
                 console.log(drawMessage(computerChoice, humanChoice));
-            } else if (computerChoice == 'scissor' && humanChoice == 'scissor') {
+            } 
+            else if (computerChoice == 'scissor' && humanChoice == 'scissor') {
                 console.log(drawMessage(computerChoice, humanChoice));
-            } else if (computerChoice == 'paper' && humanChoice == 'paper') {
+            } 
+            else if (computerChoice == 'paper' && humanChoice == 'paper') {
                 console.log(drawMessage(computerChoice, humanChoice));
-            } else if (computerChoice == 'scissor' && humanChoice == 'rock') {
-                console.log(winMessage(computerChoice, humanChoice));
-            } else if (computerChoice == 'paper' && humanChoice == 'scissor') {
-                console.log(winMessage(computerChoice, humanChoice));
-            } else if (computerChoice == 'rock' && humanChoice == 'paper') {
+            } 
+            else if (computerChoice == 'scissor' && humanChoice == 'rock') {
                 console.log(winMessage(computerChoice, humanChoice));
             } 
+            else if (computerChoice == 'paper' && humanChoice == 'scissor') {
+                console.log(winMessage(computerChoice, humanChoice));
+            } 
+            else if (computerChoice == 'rock' && humanChoice == 'paper') {
+                console.log(winMessage(computerChoice, humanChoice));
+            } 
+            if (computerScore.textContent == 5) {
+                result.textContent = 'Game ENDS! COMPUTER WINS HAHAHAHAHAHA';
+                return;
+            }
+            else if (humanScore.textContent == 5) {
+                result.textContent = 'You win... BYE';
+                return;
+            }
         }
         function playGame (){
             for (let i = 0; i<5 ; i ++) {
